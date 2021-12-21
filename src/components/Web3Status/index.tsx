39a8f42ce619666cd55a5d83e06462649b7b2840
smrnjeet222 <smrnjeet222@gmail.com>
2021-12-21 13:40:15 +0530

    feat: add gokiwallet

--- src/components/Web3Status/index.tsx
+++ src/components/Web3Status/index.tsx
@@ -1,9 +1,10 @@
 import { useWalletKit } from '@gokiprotocol/walletkit'
-import { useSolana } from '@saberhq/use-solana'
+import { useSolana, useConnectedWallet } from '@saberhq/use-solana'
 import { darken } from 'polished'
 import { Trans } from '@lingui/macro'
 import styled, { css } from 'styled-components/macro'
 import { ButtonSecondary } from '../Button'
+import { RowBetween } from '../Row'
 
 const Web3StatusGeneric = styled(ButtonSecondary)`
   ${({ theme }) => theme.flexRowNoWrap}
@@ -45,6 +46,16 @@ const Web3StatusConnect = styled(Web3StatusGeneric)<{ faded?: boolean }>`
     `}
 `
 
+const IconWrapper = styled.div<{ size?: number }>`
+  ${({ theme }) => theme.flexColumnNoWrap};
+  align-items: center;
+  justify-content: center;
+  & > * {
+    height: ${({ size }) => (size ? size + 'px' : '32px')};
+    width: ${({ size }) => (size ? size + 12 + 'px' : '32px')};
+  }
+`
+
 const Text = styled.p`
   flex: 1 1 auto;
   overflow: hidden;
@@ -58,7 +69,9 @@ const Text = styled.p`
 
 function Web3StatusInner() {
   const { connect } = useWalletKit()
-  const { disconnect, connected } = useSolana()
+  const { disconnect, connected, walletProviderInfo } = useSolana()
+  const wallet = useConnectedWallet()
+  const ICON = walletProviderInfo?.icon
 
   return !connected ? (
     <Web3StatusConnect id="connect-wallet" onClick={connect} faded={false}>
@@ -67,11 +80,24 @@ function Web3StatusInner() {
       </Text>
     </Web3StatusConnect>
   ) : (
-    <Web3StatusConnect id="connect-wallet" onClick={disconnect} faded={true}>
-      <Text>
-        <Trans>Disconnect</Trans>
-      </Text>
-    </Web3StatusConnect>
+    <RowBetween>
+      {ICON && (
+        <Text>
+          <IconWrapper size={24}>
+            {/* <img src={} alt={'WalletConnect'} /> */}
+            {/* {walletProviderInfo?.icon} */}
+            <ICON />
+          </IconWrapper>
+        </Text>
+      )}
+      <Web3StatusConnect id="connect-wallet" onClick={disconnect} faded={true}>
+        <Text>
+          <Trans>
+            {wallet?.publicKey?.toString().slice(0, 4) ?? ''}...{wallet?.publicKey?.toString().slice(-4) ?? ''}
+          </Trans>
+        </Text>
+      </Web3StatusConnect>
+    </RowBetween>
   )
 }
 
