24cee0ac4575e55f19a31f707afa7cff69ea7609
pisomanik <ajibaa@gmail.com>
2022-04-22 05:53:57 +0530

    fix: max button for SOL leaves 0.03SOL for gas

--- src/utils/maxAmountSpend.ts
+++ src/utils/maxAmountSpend.ts
@@ -1,14 +1,18 @@
 import { Currency, CurrencyAmount } from '@cykura/sdk-core'
 import JSBI from 'jsbi'
 
-const MIN_NATIVE_CURRENCY_FOR_GAS: JSBI = JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(16)) // .01 ETH
+const MIN_NATIVE_CURRENCY_FOR_GAS: JSBI = JSBI.multiply(
+  JSBI.BigInt(3),
+  JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(7))
+) // .03 SOL
+
 /**
  * Given some token amount, return the max that can be spent of it
  * @param currencyAmount to return max of
  */
 export function maxAmountSpend(currencyAmount?: CurrencyAmount<Currency>): CurrencyAmount<Currency> | undefined {
   if (!currencyAmount) return undefined
-  if (currencyAmount.currency.isNative) {
+  if (currencyAmount.currency.symbol === 'SOL') {
     if (JSBI.greaterThan(currencyAmount.quotient, MIN_NATIVE_CURRENCY_FOR_GAS)) {
       return CurrencyAmount.fromRawAmount(
         currencyAmount.currency,
