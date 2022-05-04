3ba9858e65c1264661fdc7020745fbec4265a55a
smrnjeet222 <smrnjeet222@gmail.com>
2022-05-04 21:41:44 +0530

    fix: remove analytics link - add social/docs links

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,10 @@
+import { AutoRow } from 'components/Row'
 import { StakingProvider } from 'contexts/Staking'
 import ApeModeQueryParamReader from 'hooks/useApeModeQueryParamReader'
+import { BookOpen, Send, Twitter } from 'react-feather'
 import { Route, Switch } from 'react-router-dom'
 import styled from 'styled-components/macro'
+import { ExternalLink } from 'theme'
 import GoogleAnalyticsReporter from '../components/analytics/GoogleAnalyticsReporter'
 import ErrorBoundary from '../components/ErrorBoundary'
 import Header from '../components/Header'
@@ -16,6 +19,7 @@ import RemoveLiquidityV3 from './RemoveLiquidity/V3'
 import Staking from './Staking'
 import Swap from './Swap'
 import { RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
+import DicordIcon from '../assets/svg/discord-logo.svg'
 
 const AppWrapper = styled.div`
   display: flex;
@@ -47,7 +51,12 @@ const HeaderWrapper = styled.div`
 `
 
 const Marginer = styled.div`
-  margin-top: 5rem;
+  margin-top: 3rem;
+`
+
+const LinkIconWrapper = styled.a`
+  cursor: pointer;
+  margin: 0 1rem;
 `
 
 export default function App() {
@@ -94,6 +103,23 @@ export default function App() {
             <Route component={RedirectPathToSwapOnly} />
           </Switch>
           <Marginer />
+          <AutoRow justify="center">
+            <ExternalLink href="https://docs.cykura.io/" style={{ margin: '0 1rem' }}>
+              <BookOpen size={24} />
+            </ExternalLink>
+            {/* <ExternalLink href="https://github.com/cykura" style={{ margin: '0 1rem' }}>
+              <GitHub size={24} />
+            </ExternalLink> */}
+            <ExternalLink href="https://t.me/cykuraofficialchat/" style={{ margin: '0 1rem' }}>
+              <Send size={24} />
+            </ExternalLink>
+            <ExternalLink href="https://twitter.com/cykurafi" style={{ margin: '0 1rem' }}>
+              <Twitter size={24} />
+            </ExternalLink>
+            <LinkIconWrapper href="http://discord.gg/gyaK56UreX" target="_blank">
+              <img src={DicordIcon} alt="logo" style={{ width: '28px' }} />
+            </LinkIconWrapper>
+          </AutoRow>
         </BodyWrapper>
       </AppWrapper>
     </ErrorBoundary>
